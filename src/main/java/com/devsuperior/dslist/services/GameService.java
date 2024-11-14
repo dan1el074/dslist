20b7527fa51d748cb0190db4e6df6647279e1a25
Daniel Vargas <daniel.rvargas10@gmail.com>
2024-11-14 17:14:14 -0300

    feat: setup domain model

--- src/main/java/com/devsuperior/dslist/services/GameService.java
+++ src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -15,7 +15,7 @@ public class GameService {
 
     public List<GameMinDTO> findAll() {
         List<Game> result = gameRepository.findAll();
-        List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
-        return dto;
+        List<GameMinDTO> gameDTO = result.stream().map(x -> new GameMinDTO(x)).toList();
+        return gameDTO;
     }
 }
